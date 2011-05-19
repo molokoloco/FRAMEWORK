1260d4c16fd764fb457c0fe6721cf9826cb57300
molokoloco <molokoloco@gmail.com>
2011-05-20 00:25:00 +0200

    Var cssPrefixString converted to array, retain 'Moz' pour 'MozTranform' et '' pour 'BorderRadius'

--- jquery.plugins/jquery.browsercssprefix.mlklc.js
+++ jquery.plugins/jquery.browsercssprefix.mlklc.js
@@ -1,34 +1,37 @@
-/* //////////////////////////////////////////////////////////////////////////////////////////////////////
-///// Code mixing by Molokoloco ..... 2011 ......... [EVER IN PROGRESS (it's not done yet)] ////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////// */
+/////////////////////////////////////////////////////////////////////////
+//   JS HTML5 cssPrefix By molokoloco - http://www.b2bweb.fr - 2011   //
+///////////////////////////////////////////////////////////////////////
 /*
-	// Get browser CSS style name properties (-Moz -Webkit, ... CSS support)
-	// A function to get browser specific CSS style name properties... eg. "MozBorderRadius" or "WebkitTransform" ...
-
+	// A function to get browser specific CSS style name properties...
+	// eg. "MozBorderRadius" or "WebkitTransform" ...
 	// Playground here : http://jsfiddle.net/molokoloco/f6Z3D/
 	// Source here : https://github.com/molokoloco/FRAMEWORK/blob/master/jquery.plugins/jquery.browsercssprefix.mlklc.js
 	
 	// Usages examples
 	
 	var cssTransform = cssPrefix('Transform'); // "MozTransform" or "WebkitTransform"
-	if (cssTransform) {
+	if (cssTransform ) {
 		var cssProp = {};
 		cssProp['border'] = '1px solid rgba(0, 0, 0, .5)';
 		cssProp[cssTransform] = 'rotate(20deg)';
-		cssProp[cssPrefix('borderRadius')] = '5px'; // Keep the camelCaze
-		$('#myDiv').css(cssProp);
+		cssProp[cssPrefix('borderRadius')] = '5px'; // Keep the camelCaze (jQuery like)
+		cssProp[cssPrefix('boxShadow')] = '2px 2px 2px grey';
+		$('div#myDiv').css(cssProp);
+		// console.log(cssProp);
 	}
+	
+	// Want more code ? That here...
+	// https://github.com/molokoloco/FRAMEWORK/
 */
 
-
-var cssPrefixString = null;
+var cssPrefixString = {};
 var cssPrefix = function(propertie) {
-    if (cssPrefixString !== null) return cssPrefixString + propertie;
+    if (cssPrefixString[propertie]) return cssPrefixString[propertie] + propertie;
     var e = document.createElement('div');
     var prefixes = ['', 'Moz', 'Webkit', 'O', 'ms', 'Khtml']; // Various supports...
     for (var i in prefixes) {
-        if (typeof e.style[prefixes[i] + propertie] != 'undefined') {
-            cssPrefixString = prefixes[i];
+        if (typeof e.style[prefixes[i] + propertie] !== 'undefined') {
+            cssPrefixString[propertie] = prefixes[i];
             return prefixes[i] + propertie;
         }
     }
