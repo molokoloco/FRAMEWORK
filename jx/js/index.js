bfc8db5b14843246b3be124d62b6135c9df070b8
molokoloco <molokoloco@gmail.com>
2011-07-11 23:29:30 +0200

    jQuery Presentation SlidR V1.2

--- jx/js/index.js
+++ jx/js/index.js
@@ -1,5 +1,5 @@
 /*
-	// SlidR - copyleft Molokoloco 2011
+	// SlidR - copyleft Molokoloco 2011 - Version alpha 1.2
 	// Demo : http://www.b2bweb/bonus/jx/
 	// The sources here : https://github.com/molokoloco/FRAMEWORK/tree/master/jx/
 */
