d80b792257874af2e53acb255411a559b69ceee3
molokoloco <molokoloco@gmail.com>
2011-10-11 01:44:09 +0200

    jQuery Analogue Clock plugin V2.1 Signed-off-by: molokoloco <molokoloco@gmail.com>

--- jquery.plugins/jquery.analogueClock2.mlklc.js
+++ jquery.plugins/jquery.analogueClock2.mlklc.js
@@ -30,24 +30,34 @@
     });
 */
 
-;(function($, window, document, undefined ) {
+/*
+    // Pixel polished jQuery & CSS3 Analogue Clock plugin V2.1
+    // by molokoloco@gmail.com 10/10/2011 - 
+    // Infos : http://www.b2bweb.fr/molokoloco/pixels-polished-jquery-css3-analogue-clock/
+    // jsFiddle : http://jsfiddle.net/molokoloco/V2rFN/
+*/
+
+;(function($, window) {
     $.fn.extend({ // Extend jQuery with custom plugin
-        // Cross browser rotate
+        
+        // Cross browser CSS rotate
         rotate: function(val) { // $('div').rotate('90deg');
             return this.each(function() {
                 var rotate = 'rotate('+val+')';
                 return $(this).css({'-moz-transform':rotate, '-webkit-transform':rotate, '-ms-transform':rotate, '-o-transform':rotate, transform:rotate});
             });
         },
-        // Distribute items clockwise
+        
+        // Distribute elements clockwise inside a box
         circalise: function(options) { // $('div').circalise({targets:'div.unit'});
             options = $.extend({
                 targets          :'> *', // childs elements to distribute inside this box
                 rotateTargets    :false,
                 startAngle       :270, // 270deg, start at top center (like a clock)
                 xRadius          :null, // default radius to the radius of the box, minus target width
-				yRadius          :null
+                yRadius          :null
             }, options || {});
+            
             return this.each(function() {  
                 var $this = $(this),
                     $targets = $this.find(options.targets),
@@ -58,44 +68,51 @@
                     var $target = $(this),
                         xCenter = (parseInt($this.innerWidth(), 10) - parseInt($target.outerWidth(), 10)) / 2,
                         yCenter = (parseInt($this.innerHeight(), 10) - parseInt($target.outerHeight(), 10)) / 2,
-                        xRadius = (options.xRadius ? options.xRadius : xCenter),
-                        yRadius = (options.yRadius ? options.yRadius : yCenter),
+                        xRadius = (options.xRadius || options.xRadius === 0 ? options.xRadius : xCenter),
+                        yRadius = (options.yRadius || options.yRadius === 0 ? options.yRadius : yCenter),
                         params = {
                             left: xRadius * Math.cos(angle) + xCenter,
                             top: yRadius * Math.sin(angle) + yCenter
                         };
                     $target.css(params);
                     if (options.rotateTargets)
-                        $target.rotate((Math.atan2(params.top-yCenter, params.left-xCenter)+1.5707963267948966)+'rad'); // 1.57 == 90° rotation to keep tangent
+                        $target.rotate((Math.atan2(params.top-yCenter, params.left-xCenter)+(Math.PI/2))+'rad'); // (Math.PI/2) == 90deg in rad : rotate to keep tangent
                     angle += increase;
                 });
             });
          },
-         // Analogue Clock plugin
+         
+        // Analogue Clock plugin
          analogueClock: function(options) {
-            options = $.extend({ // Default values
+             options = $.extend({ // Default values
                 withHours        :true, // Print digit time ?
                 rotateHours      :true, // Rotate digit time ?
                 withUnits        :true, // Print unit ?
-                withDigitalTime  :true // Print time (digital) in center
+                withDigitalTime  :false, // Print time (digital) in center
+                withDate         :false // Print date
             }, options || {});  
-            return this.each(function() {
-                // Build clock
+            
+             return this.each(function() {
+                
+                 // Build clock
                 var $clock = $(this),
-					clockW = $clock.width(), clockH = $clock.height(),
+                    clockW = $clock.width(), clockH = $clock.height(),
                     $sec = $('<div class="sec"><div class="clockwise"></div></div>').appendTo($clock),
                     $min = $('<div class="min"><div class="clockwise"></div></div>').appendTo($clock),
                     $hour = $('<div class="hour"><div class="clockwise"></div></div>').appendTo($clock),
                     $time = (options.withDigitalTime ? $('<div class="time">00:00:00</div>').appendTo($clock) : null),
+                    $date = (options.withDate ? $('<div class="date"> XXX </div>').appendTo($clock) : null),
                     $innerCenter = $('<div class="innerCenter"></div>').appendTo($clock),
-					$clockGlass = $('<div class="clockGlass"></div>').appendTo($clock);
-				// CSS Center elements with half clock diameter
-				$sec.css({top:0, left:( (clockW/2)-(parseInt($sec.width(), 10) / 2) )+'px', height:clockH+'px'});
-				$min.css({top:0, left:( (clockW/2)-(parseInt($min.width(), 10) / 2) )+'px', height:clockH+'px'});
-				$hour.css({top:0, left:( (clockW/2)-(parseInt($hour.width(), 10) / 2) )+'px', height:clockH+'px'});
-				$clock.find('div.clockwise').css({bottom:(clockH/2)+'px'}); // center of the #clock inside hour,min,sec
-				$innerCenter.css({margin:'-'+(parseInt($innerCenter.height(), 10)/2)+'px 0 0 -'+(parseInt($innerCenter.width(), 10)/2)+'px'});
-				// Built analog digits number
+                    $clockGlass = $('<div class="clockGlass"></div>').appendTo($clock);
+                
+                // CSS Center elements with half clock diameter
+                $sec.css({top:0, left:( (clockW/2)-(parseInt($sec.width(), 10) / 2) )+'px', height:clockH+'px'});
+                $min.css({top:0, left:( (clockW/2)-(parseInt($min.width(), 10) / 2) )+'px', height:clockH+'px'});
+                $hour.css({top:0, left:( (clockW/2)-(parseInt($hour.width(), 10) / 2) )+'px', height:clockH+'px'});
+                $clock.find('div.clockwise').css({bottom:(clockH/2)+'px'}); // center of the #clock inside hour,min,sec
+                $innerCenter.css({margin:'-'+(parseInt($innerCenter.height(), 10)/2)+'px 0 0 -'+(parseInt($innerCenter.width(), 10)/2)+'px'});
+                
+                // Built analog digits number
                 if (options.withHours) {
                     var plotsNum = 12, // 12 hours digits, normally ^^
                         digitsHtml = '';
@@ -106,6 +123,7 @@
                     $clock.append(digitsHtml);
                     $clock.circalise({targets:'div.digit', rotateTargets:options.rotateHours});
                 }
+                
                 // Built analog digits number
                 if (options.withUnits) {
                     var plotsNum = 12, // 12 hours digits, normally ^^
@@ -114,12 +132,12 @@
                     $clock.append(unitsHtml);
                     var xRadius = parseInt($innerCenter.width(), 10) / 2
                                - (parseInt($clock.find('div.unit').eq(0).outerHeight(), 10) / 2),
-						yRadius = parseInt($innerCenter.height(), 10) / 2
+                        yRadius = parseInt($innerCenter.height(), 10) / 2
                                - (parseInt($clock.find('div.unit').eq(0).outerHeight(), 10) / 2);;
                     $clock.circalise({targets:'div.unit', rotateTargets:true, xRadius:xRadius, yRadius:yRadius});
                 }
+                
                 // Animate clockwise
-				Number.prototype.padTime = function() { return (this < 10 ? '0'+this : this); };
                 var timer = function() {
                     var now = new Date(),
                         seconds = now.getSeconds(),
@@ -128,11 +146,14 @@
                     $sec.rotate((seconds * 6)+'deg'); // 60 * 6 == 360°
                     $min.rotate((mins * 6)+'deg');
                     $hour.rotate((hours * 30 + (mins / 2))+'deg');
-                    if (options.withDigitalTime)
-                        $time.html(hours.padTime()+':'+mins.padTime()+':'+seconds.padTime());
-                    setTimeout(timer, 300); // precision 300ms
+                    if (options.withDate) // Tue Oct 11 2011 00:37:36 GMT+0200 (Paris, Madrid (heure d'été))
+                        $date.html(now.toString().split(now.getFullYear())[0]);
+                    if (options.withDigitalTime) 
+                        $time.html(now.toString().split(now.getFullYear())[1].split(' ')[1]);
+                    setTimeout(timer, 300); // precision 300ms is fine when widget is alone ;)
                 };
                 timer(); // init !
+                
                 return $(this);
             });
         }
