2395c6a4ba9765a7388bf65b7b6ec65cf076bb14
Julien G <molokoloco@gmail.com>
2013-07-06 16:41:55 +0200

    V0.9.7 really improved perfs for big pages

--- jquery.plugins/jquery.colonizr.js
+++ jquery.plugins/jquery.colonizr.js
@@ -39,28 +39,6 @@
     });
 
 * ============================================================== */
-// REQUEST ANIMATION FRAME
-if (!('requestAnimationFrame' in window)) {
-    var lastTime = 0,
-        vendors = ['webkit', 'moz', 'ms', 'o'];
-
-    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-    }
-
-    if (!('requestAnimationFrame' in window)) {
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime(),
-                timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-                id = window.setTimeout(function () {
-                    callback(currTime + timeToCall);
-                }, timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
-}
-
 
 !function ($) {
 
