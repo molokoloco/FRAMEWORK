99febfe1ae844066ab35f7847aacb6f394a0e4ee
Julien G <molokoloco@gmail.com>
2013-07-07 16:43:36 +0200

    jQuery colonizr V0.9.9
    I hope this is as robust as fast for now :)

--- jquery.plugins/jquery.colonizr.js
+++ jquery.plugins/jquery.colonizr.js
@@ -1,11 +1,11 @@
 /* =============================================================
  *
- * jQuery colonizr V0.9.8 - Molokoloco 2013 - Copyleft
+ * jQuery colonizr V0.9.9 - Molokoloco 2013 - Copyleft
  * "In-between titles Multicols paragraphes" (Bootstrap-like plugin)
  *
- * Blog post : http://www.b2bweb.fr/molokoloco/jquery-colonize-plugin-in-between-titles-multicols-paragraphes-with-css3/
  * jsFiddle demo : http://jsfiddle.net/molokoloco/FK7xr/
  * Real life demo : http://www.b2bweb.fr/gueznet/27062013-gueznet-prism-javascript-networks-politics-350-links/
+ * Blog post : http://www.b2bweb.fr/molokoloco/jquery-colonize-plugin-in-between-titles-multicols-paragraphes-with-css3/
  *
  * Github :
  *   - https://github.com/molokoloco/FRAMEWORK/blob/master/jquery.plugins/jquery.colonizr.js
@@ -16,28 +16,33 @@
 
 /* =============================================================
 
-    // Usage example... (+ 10 000 chars will be very long to process)
+    // Usage example... (+ 10 000 chars will be long to process)
 
-    var $container = $('div#container');
+    var $container = $('div#container'); // Main box
 
-    $('a#colonize').click(function() {     // Call on click
-        $container.colonizr({              // Use plugin...
-            chapters:   'h2,h3',
-            take:       'p,ul,quote',      // Adding UL and quote to the stream...
+    $('a#colonize').one(function() {     // Call on click
+        
+        $container.on('colonizrFinish', function() {
+            console.log('colonizr complete');
+        });
+
+        $container.colonizr({
+            chapters:   'h2,h3',           // The chapters will not be wrapped inside cols
+            take:       'p,ul,quote',      // In--between chapters tags we take, adding UL and quote to the stream...
             css:        'multiplecolumns'  // If you want to change the CSS..
         });
     });
 
     var windowTmr = null; // Timeout...
-    var resizeRefreshEvent = function() {   // Trottle resize...
+    var resizewrapAllEvent = function() {   // Trottle resize...
         windowTmr = null;
         if ($container.data('colonizr'))    // colonizr was applyed by user click ?
-            $container.colonizr('refresh'); //  Refresh cols height...
+            $container.colonizr('wrapAll'); //  wrapAll cols height...
     };
 
     $(window).on('resize', function(event) { // Resize Event
         if (windowTmr) clearTimeout(windowTmr);
-        windowTmr = setTimeout(resizeRefreshEvent, 1600); // Trottle resize
+        windowTmr = setTimeout(resizewrapAllEvent, 1500); // Trottle resize
     });
 
 * ============================================================== */
@@ -54,23 +59,40 @@
         this.$container = $(element);
         this.wrapper    = '<div class="'+this.options.css+'"/>';
         this.cWidth, this.intentNextP, this.lineMinHeight, this.maxHeight, this.chaptersTotal;
-        this.refresh();
+        this.wrapAll();
     }
 
     Colonizr.prototype = {
 
         constructor: Colonizr,
 
-        colsExtractor: function (i, $element) {  // Cannot be done with $.wrapAll() || $.nextAll() // :-(
+        wrapCleaner: function() {
+            var $exists = this.$container.find('.'+this.options.css);
+            if (!$exists.length) return;
+            // Existing this.wrappers ?
+            var $parent = this.$container.parent(),
+                $prev   = this.$container.prev();
+            this.$container.detach(); // Detach DOM
+            $exists.each(function() { // Unwrapping cols
+                var $this = $(this);
+                $($this.html()).insertBefore($this);
+                $this.remove();
+            });
+            if ($prev.length) this.$container.insertAfter($prev);
+            else this.$container.appendTo($parent);
+        },
 
-             var $next          = $element.next(),
+        wrapBrothers: function (i, $element) {  // Cannot be done with $.wrapAll() || $.nextAll() // :-(
+
+            var $next          = $element.next(),
                 $collection    = [],
                 jumpNext       = false,
                 totalHeight    = 0,
                 estimateHeight = 0;
 
-             while ($next.length) {
+             while ($next.length) { // Iterate next sibling
                 if ($next.is(this.options.take)) {
+                    estimateHeight += $next.data('h'); // Work fine if "p" margin (2) == ".multiplecolumns > p" margin (2 * nb cols)
                     $collection.push($next);
                     $next = $next.next();
                 }
@@ -88,13 +110,7 @@
                 }
             }
 
-            if ($collection.length) {
-                for (var j = 0, len = $collection.length; j < len; j++) {
-                    estimateHeight += $collection[j].data('h'); // Work fine if "p" margin (2) == ".multiplecolumns > p" margin (2 * nb cols)
-                }
-            }
-
-            if ($collection.length && estimateHeight > this.lineMinHeight) {
+            if ($collection.length && estimateHeight > this.lineMinHeight) { // WRAP ?
                 var $wrapper = $(this.wrapper);
                 for (var j = 0, len = $collection.length; j < len; j++) {
                     if (totalHeight < 1 && ($collection[j].html() == '&nbsp;' || $collection[j].is('br'))) {
@@ -115,32 +131,16 @@
             }
 
             if (jumpNext) { 
-                this.colsExtractor(i, $next);
+                this.wrapBrothers(i, $next);
             }
             else if ((i+1) == this.chaptersTotal) {
-                this.$container.trigger('finish');
+                this.$container.trigger('wrapBrothersFinish');
             }
         },
 
-        cleanColumns: function() {
-            var $exists = this.$container.find('.'+this.options.css);
-            if (!$exists.length) return;
-            // Existing this.wrappers ?
-            var $parent = this.$container.parent(),
-                $prev   = this.$container.prev();
-            this.$container.detach(); // Detach DOM
-            $exists.each(function() {
-                var $this = $(this);
-                $($this.html()).insertBefore($this);
-                $this.remove();
-            });
-            if ($prev.length) this.$container.insertAfter($prev);
-            else this.$container.appendTo($parent);
-        },
+        wrapAll: function () {
 
-        refresh: function () {
-
-            this.cleanColumns();
+            this.wrapCleaner();
 
             var that      = this,
                 $chapters = that.$container.find(that.options.chapters),
@@ -160,12 +160,14 @@
             that.lineMinHeight = $p.outerHeight() * that.options.minLine;
             $p.remove();
 
-            that.$container.on('finish', function() {
+            that.$container.on('wrapBrothersFinish', function() {
+                // Re-attach container...
                 if ($prev.length) that.$container.insertAfter($prev);
                 else that.$container.appendTo($parent);
+                that.$container.trigger('colonizrFinish'); // If somebody outside listen our asynchronous finish
             });
 
-            $chapters  //  We cannot detach the container before operating because we need the height of some elements inside
+            $chapters // Before detaching the container to operate, we need the height of some elements inside
                 .each(function(i, e) {
                     var $next = $(e).next();
                     while ($next.length) {
@@ -173,20 +175,20 @@
                             $next = []; // Break
                         }
                         else  {
-                            if ($next.length && $next.is(that.options.take)) $next.data('h', $next.outerHeight());
+                            if ($next.length && $next.is(that.options.take)) $next.data('h', $next.outerHeight()); // Stock each height
                             if ($next.length) $next = $next.next();
                         }
                     }
                 });
 
-            that.$container = that.$container.detach(); // Big optim for big page ;)
+            // Big optim for big page ;)
+            that.$container = that.$container.detach(); 
 
-            $chapters.each(function(i, e) {
-                //setTimeout($.proxy(that.colsExtractor, that), 0, i, $(e)); // Deferred for the so long #GuezNet page !
-                //window.requestAnimationFrame(that.colsExtractor.bind(that, i, $(e)));
-                that.colsExtractor(i, $(e));
+            $chapters.each(function(i, e) { // Let's go recursive
+                //setTimeout($.proxy(that.wrapBrothers, that), 0, i, $(e)); // Deferred for the so long #GuezNet page !
+                //window.requestAnimationFrame(that.wrapBrothers.bind(that, i, $(e)));
+                that.wrapBrothers(i, $(e));
             });
-
         }
     };
 
